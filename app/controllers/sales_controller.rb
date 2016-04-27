07228e96a98aa2723032c28cac81c97141ed7138
Rowan Cota <cota.rg@gmail.com>
2016-04-27 14:37:25 -0700

    added permitted params methods.

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -8,6 +8,12 @@ class SalesController < ApplicationController
 	def create
 	end
 
-	
+  def sales_create_params
+    params.permit(sale: [:name, ]) # these need to be finished with which params need to be let through
+  end
+
+  def sales_update_params
+    params.permit(sale: [:name, ]) # these need to be finished with which params need to be let through
+  end
 
 end
