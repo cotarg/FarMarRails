b313530ed436da6f31e8b71dbdf47da4c7c75a2a
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-04-27 09:50:51 -0700

    Markets controller complete

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,45 @@
 class MarketsController < ApplicationController
+
+  def index #VIEWED BY USER
+    @markets = Market.all
+    render :index
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    @market = Market.new(create_market_params[:market])
+    #unknown if need other things attached, like vendor/sales/products
+    if @market.save
+      @markets = Market.all
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @market = Market.find(params[:market_id])
+    render :new
+  end
+
+  def show
+    @market = Market.find(params[:market_id])
+    render :show
+  end
+
+  def update
+    @market = Market.find(params[:market_id]).update(create_market_params[:market])
+    @markets = Market.all
+    render :index  #IF time at end, render a "newly created" view for proofread, avoid list of all markets / index, because markets aren't supposed to see other markets
+  end
+
+  def destroy
+    @market = Market.find(params[:market_id]).destroy
+    @markets = Market.all
+    render :index  #Same as update: If time at end, render not-index
+  end
+  
 end
