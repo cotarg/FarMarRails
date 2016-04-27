2a7ab499dfe53c639322536834fb33cd74abb57f
Rowan Cota <cota.rg@gmail.com>
2016-04-27 14:39:01 -0700

    added basic methods that will be needed for products to controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,20 @@
 class ProductsController < ApplicationController
+
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+  def product_create_params
+    params.permit(product: [:name, ]) # these need to be finished with which params need to be let through
+  end
+
+  def product_update_params
+    params.permit(product: [:name, ]) # these need to be finished with which params need to be let through
+  end
+
 end
