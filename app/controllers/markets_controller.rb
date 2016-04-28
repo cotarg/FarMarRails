f2f1389b16f4835d2e13c837505ddf7f316d492e
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-04-28 13:15:31 -0700

    Updated gemfile to accomodate potential testing, new market form functional aside from hidden field setting market_id

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -7,6 +7,7 @@ class MarketsController < ApplicationController
   end
 
   def new
+    @market = Market.new
     render :new
   end
 
@@ -15,14 +16,14 @@ class MarketsController < ApplicationController
     #unknown if need other things attached, like vendor/sales/products
     if @market.save
       @markets = Market.all
-      redirect_to :index
+      redirect_to markets_path
     else
       render :new
     end
   end
 
   def edit
-    @market = Market.find(params[:market_id])
+    @market = Market.find(params[:id])
     render :new
   end
 
@@ -32,13 +33,13 @@ class MarketsController < ApplicationController
   end
 
   def update
-    @market = Market.find(params[:market_id]).update(create_market_params[:market])
+    @market = Market.find(params[:id]).update(create_market_params[:market])
     @markets = Market.all
     render :index  #IF time at end, render a "newly created" view for proofread, avoid list of all markets / index, because markets aren't supposed to see other markets
   end
 
   def destroy
-    @market = Market.find(params[:market_id]).destroy
+    @market = Market.find(params[:id]).destroy
     @markets = Market.all
     render :index  #Same as update: If time at end, render not-index
   end
