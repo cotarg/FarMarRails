e735d981bbd933b637de077e8b608140963403f2
Rowan Cota <cota.rg@gmail.com>
2016-04-29 08:47:07 -0700

    added placeholder until merge.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,8 +1,10 @@
 class VendorsController < ApplicationController
 
-	def index
-		@vendors = Vendor.select(:id, :name).order(:name)
-		render :login
-	end
+	# def index
+	# 	@vendors = Vendor.select(:id, :name).order(:name)
+	# 	render :login
+	# end
+
+	
 
 end
